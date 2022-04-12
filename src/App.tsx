9b852adbc74b1a0989222dabe5471971dfd2884b
Oleg <viktorovich959595@mail.ru>
2022-04-12 20:09:10 +1000

    added save to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,6 @@ function App() {
     }
     const onSetClick = () => {
         dispatch(setValuesInCounterAC(startValue, maxValue))
-        console.log('Записал в локалстор')
     }
 
     return (
